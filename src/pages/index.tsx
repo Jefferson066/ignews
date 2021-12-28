001ef9629432a7a087f200a7358e87585f526acb
Jefferson Santos <jeffersonscr12@gmail.com>
2021-12-28 13:53:25 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inico | Ig.news</title>
       </Head>
-      <h1 >
-        Hello <span>world</span>
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome</span>
+          <h1>News about the <span>React</span>world.</h1>
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+       {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/images/avatar.svg" alt="Girl coding"  />
+      </main>
+
     </>
   )
 }
