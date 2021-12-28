8690ec9afb4bf5b19fddc64a4ff605d1bd7319dc
Jefferson Santos <jeffersonscr12@gmail.com>
2021-12-28 14:04:37 -0300

    subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -14,12 +15,13 @@ export default function Home() {
           <span>Hey, Welcome</span>
           <h1>News about the <span>React</span>world.</h1>
           <p>
-            Get acess to all the publications <br/>
+            Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
-       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/images/avatar.svg" alt="Girl coding"  />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
 
     </>
