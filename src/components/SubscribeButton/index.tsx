1b18893134c6261b8e847f2e1f17c2772ca4feb4
Jefferson Santos <jeffersonscr12@gmail.com>
2021-12-28 15:31:25 -0300

    stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,11 @@
 
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface ISubscribeButtonProps{
+    priceId: string,
+}
+
+export function SubscribeButton({ priceId}: ISubscribeButtonProps) {
    
     return(
         <button
